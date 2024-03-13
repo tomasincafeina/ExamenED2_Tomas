3d22e8126af00ff813a7c5309db96f642027474d
“Tomas” <damj03.2024@gmail.com>
2024-03-13 17:12:24 +0100

    Proyecto documentado

--- src/examen/Fecha.java
+++ src/examen/Fecha.java
@@ -1,1 +1,36 @@
-package examen;public class Fecha{public static boolean validarFecha(int dia, int mes, int anio){boolean resultado = false;if ((dia>0)&&(mes>0)&&(anio>0)) {if ((mes <=12)&&(dia <=31)) {	if (mes ==2) {resultado = dia <=28;}else if (mes%2 == 0) {resultado = dia <=30;}else {resultado = dia <=31;}}}return resultado;}}
+package examen;
+/**
+ * Esta clase calculo por medio de unas condiciones si la fecha introducida es valida o no.
+ * 
+ * @author Tomas
+ * @version 1.0
+ */
+public class Fecha {
+	/**
+	 * Declaracion la variable fechaValida de tipo statico booleano.
+	 */
+	public static boolean fechaValida;
+	/**
+	 * Este método publico pide los parametros de la fecha y calucla si es valido o no.
+	 * 
+	 * @param dia
+	 * @param mes
+	 * @param anio 
+	 * @return Devuelve un valor booleano, true o false.
+	 */
+	public static boolean validarFecha(int anio, int mes, int dia) {
+		fechaValida = false;
+		if ((dia > 0) && (mes > 0) && (anio > 0)) {
+			if ((mes <= 12) && (dia <= 31)) {
+				if (mes == 2) {
+					fechaValida = dia <= 28;
+				} else if (mes % 2 == 0) {
+					fechaValida = dia <= 30;
+				} else {
+					fechaValida = dia <= 31;
+				}
+			}
+		}
+		return fechaValida;
+	}
+}
