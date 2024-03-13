470a59b7f161b82d847e7316bdf9a03595d72ec2
“Tomas” <damj03.2024@gmail.com>
2024-03-13 17:08:50 +0100

    Proyecto sin documentar

---
+++ src/examen/Fecha.java
@@ -0,0 +1,1 @@
+package examen;public class Fecha{public static boolean validarFecha(int dia, int mes, int anio){boolean resultado = false;if ((dia>0)&&(mes>0)&&(anio>0)) {if ((mes <=12)&&(dia <=31)) {	if (mes ==2) {resultado = dia <=28;}else if (mes%2 == 0) {resultado = dia <=30;}else {resultado = dia <=31;}}}return resultado;}}
---
+++ src/examen/FechaTest.java
@@ -0,0 +1,10 @@
+package examen;
+
+public class FechaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
